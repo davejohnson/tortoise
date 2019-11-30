55ebe55fbd6fe365668c483fb6d311ed062e310f
Dave Johnson <dave.c.johnson@gmail.com>
2019-11-30 13:59:14 -0800

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -10,8 +10,8 @@ exports.handler = async function Index () {
     fullname: '🐢tortoise capital', // ←  Start by adding your name!
     title: 'tortoise capital',
     occupation: '',
-    location: 'Vancouver, BC',
-    bio: 'We focus on building, acquiring, and running small SaaS businesses.\\n\\nInstead of growth at any cost, which leads to constant fire fighting, questionable business practices, and worse. Our path is about small, profitable, low overhead businesses.\\n\\nWe trade building a hiring practice for a fungible group of well paid, remote work, contractors.\\n\\nInstead of trying to discover new marketing channels for each stage of growth, we build products to work with a handful of known channels.\\n\\nIssues like technical debt and product architecture are minimized by constraining features and knowing that all products have a shelf life.\\n\\nThe constraints of building small frees our creativity.',
+    location: '',
+    bio: 'We focus on building, acquiring, and running small SaaS businesses.<br><br>Instead of growth at any cost, which leads to constant fire fighting, questionable business practices, and worse. Our path is about small, profitable, low overhead businesses.<br><br>We trade building a hiring practice for a fungible group of well paid, remote work, contractors.<br><br>Instead of trying to discover new marketing channels for each stage of growth, we build products to work with a handful of known channels.<br><br>Issues like technical debt and product architecture are minimized by constraining features and knowing that all products have a shelf life.<br><br>The constraints of building small frees our creativity.',
 
     /**
      * Contact / social
